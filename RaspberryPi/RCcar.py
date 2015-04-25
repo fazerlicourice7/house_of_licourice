3df10c6761d875a786014d07186a0f506b50b983
Vamshi Balanaga <vamrocks602@gmail.com>
2015-04-25 17:12:31 +0000

    tried multiple different ways of continuous keybaord input to no avail

--- RaspberryPi/RCcar.py
+++ RaspberryPi/RCcar.py
@@ -1,20 +1,23 @@
-import pygame, RCcarFunctions, time
+import pygame, RCcarFunctions
 pygame.init()
 RCcarFunctions.init()
 try:
   while True:
    #RCcarFunctions.forward()
     keypressed = pygame.key.get_pressed()
-    if keypressed == "pygame.K_w":
+    #for event in pygame.event.get():
+     # if event.type == pygame.KEYDOWN:
+    if keypressed[pygame.K_w]:
       RCcarFunctions.forward()
-    elif keypressed  == "pygame.K_a":
+    elif keypressed[pygame.K_a]:
       RCcarFunctions.spinleft()
-    elif keypressed == "pygame.K_s":
+    elif keypressed[pygame.K_s]:
       RCcarFunctions.reverse()
-    elif keypressed == "pygame.K_d":
+    elif keypressed[pygame.K_d]:
       RCcarFunctions.spinright()
-    elif keypressed == "pygame.K_ESCAPE": 
+    else: 
       RCcarFunctions.stop()
     pygame.event.pump()
 except KeyboardInterrupt:
+  print ("\ncleaning up")
   RCcarFunctions.cleanup()
