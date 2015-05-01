dddad10908517118dac611abb248bf60c77f1c81
Vamshi Balanaga <vamrocks602@gmail.com>
2015-05-01 23:07:14 +0000

    added viable keybaord reading segment

--- RaspberryPi/RCcar.py
+++ RaspberryPi/RCcar.py
@@ -1,23 +1,59 @@
 import pygame, RCcarFunctions
-pygame.init()
+
+#keyboard reading stuff-stll don't understand it 
+import sys
+import tty
+import termios
+
+UP = 0
+DOWN = 1
+RIGHT = 2
+LEFT = 3
+
+def readchar():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    if ch == '0x03':
+        raise KeyboardInterrupt
+    return ch
+
+def readkey(getchar_fn=None):
+    getchar = getchar_fn or readchar
+    c1 = getchar()
+    if ord(c1) != 0x1b:
+        return c1
+    c2 = getchar()
+    if ord(c2) != 0x5b:
+        return c1
+    c3 = getchar()
+    return ord(c3) - 65  # 0=Up, 1=Down, 2=Right, 3=Left arrows
+# end of keyboard reading stuff
+
+#pygame.init()
 RCcarFunctions.init()
+keypressed = readkey()
 try:
   while True:
    #RCcarFunctions.forward()
-    keypressed = pygame.key.get_pressed()
+    #keypressed = pygame.key.get_pressed()
     #for event in pygame.event.get():
      # if event.type == pygame.KEYDOWN:
-    if keypressed[pygame.K_w]:
+    if keypressed == 'w':
       RCcarFunctions.forward()
-    elif keypressed[pygame.K_a]:
+    elif keypressed == 'a':
       RCcarFunctions.spinleft()
-    elif keypressed[pygame.K_s]:
+    elif keypressed == 's':
       RCcarFunctions.reverse()
-    elif keypressed[pygame.K_d]:
+    elif keypressed == 'd':
       RCcarFunctions.spinright()
-    else: 
+    elif keypressed == ' ': 
       RCcarFunctions.stop()
-    pygame.event.pump()
+    #pygame.event.pump()
 except KeyboardInterrupt:
   print ("\ncleaning up")
   RCcarFunctions.cleanup()
