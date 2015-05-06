9227505363f0183b3d66f95d4267803c7e738e8d
Vamshi Balanaga <vamrocks602@gmail.com>
2015-05-06 16:10:07 -0400

    modified input of RCcar.py

--- RaspberryPi/RCcar.py
+++ RaspberryPi/RCcar.py
@@ -1,59 +1,49 @@
 import pygame, RCcarFunctions
 
-#keyboard reading stuff-stll don't understand it 
-import sys
-import tty
-import termios
-
-UP = 0
-DOWN = 1
-RIGHT = 2
-LEFT = 3
-
-def readchar():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    if ch == '0x03':
-        raise KeyboardInterrupt
-    return ch
-
-def readkey(getchar_fn=None):
-    getchar = getchar_fn or readchar
-    c1 = getchar()
-    if ord(c1) != 0x1b:
-        return c1
-    c2 = getchar()
-    if ord(c2) != 0x5b:
-        return c1
-    c3 = getchar()
-    return ord(c3) - 65  # 0=Up, 1=Down, 2=Right, 3=Left arrows
-# end of keyboard reading stuff
-
-#pygame.init()
+pygame.init()
 RCcarFunctions.init()
-keypressed = readkey()
+global forward, reverse, left, right
+forward = False
+left = False
+reverse = False
+right = False
 try:
   while True:
    #RCcarFunctions.forward()
     #keypressed = pygame.key.get_pressed()
-    #for event in pygame.event.get():
-     # if event.type == pygame.KEYDOWN:
-    if keypressed == 'w':
+    for event in pygame.event.get():
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_w:
+          forward = True
+        elif event.key == pygame.K_a:
+          left = True
+        elif event.key == pygame.K_s:
+          reverse = True 
+        elif event.key == pygame.K_d:
+          right = True
+      elif event.type == pygame.KEYUP:
+        if event.key == pygame.K_w:
+          forward = False
+        elif event.key == pygame.K_a:
+          left = False
+        elif event.key == pygame.K_s:
+          reverse = False
+        elif event.key == pygame.K_d:
+          right = False
+    if forward & right:
+      RCcarFunctions.right()
+    elif forward & left:
+      RCcarFunctions.left()
+    elif forward:
       RCcarFunctions.forward()
-    elif keypressed == 'a':
+    elif left:
       RCcarFunctions.spinleft()
-    elif keypressed == 's':
+    elif reverse:
       RCcarFunctions.reverse()
-    elif keypressed == 'd':
-      RCcarFunctions.spinright()
-    elif keypressed == ' ': 
+    elif right:
+      RCcarFunctions.right()
+    else:
       RCcarFunctions.stop()
-    #pygame.event.pump()
 except KeyboardInterrupt:
   print ("\ncleaning up")
   RCcarFunctions.cleanup()
