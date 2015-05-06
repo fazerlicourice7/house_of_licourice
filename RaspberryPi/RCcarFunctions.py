7a94f3ce8d291b75432543d2c4cd6162e22ddcf5
Vamshi Balanaga <vamrocks602@gmail.com>
2015-05-06 20:38:11 +0000

    added new file to test car functions

--- RaspberryPi/RCcarFunctions.py
+++ RaspberryPi/RCcarFunctions.py
@@ -9,67 +9,72 @@ R1 = 24  #right motor: connection 1
 
 #initializes required gpio pins as outputs 
 def init():
+  global L01, L10, R01, R10
   gpio.setmode(gpio.BCM)  # using the official GPIO numbering
   gpio.setwarnings(False)  
   gpio.setup(L0, gpio.OUT)
-  l0 = gpio.PWM(L0, 100)  
+  L01 = gpio.PWM(L0, 100)  
   gpio.setup(L1, gpio.OUT)
-  l1 = gpio.PWM(L1, 100)
+  L10 = gpio.PWM(L1, 100)
   gpio.setup(R0, gpio.OUT)
-  r0 = gpio.PWM(R0, 100)
+  R01 = gpio.PWM(R0, 100)
   gpio.setup(R1, gpio.OUT)
-  r1 = gpio.PWM(R1, 100)
-
+  R10 = gpio.PWM(R1, 100)
+  #start all the outputs at 0% duty cycle
+  L01.start(0)
+  L10.start(0)
+  R01.start(0)
+  R10.start(0)
 #creates a function which makes the car go forward
 def forward():
-  l0.start(100)
-  l1.start(0)
-  r0.start(100)
-  r1.start(0)
+  L01.ChangeDutyCycle(100)
+  L10.ChangeDutyCycle(0)
+  R01.ChangeDutyCycle(100)
+  R10.ChangeDutyCycle(0)
   
 #function that makes the car reverse
 def reverse():
-  l0.ChangeDutyCycle(0)
-  l1.ChangeDutyCycle(100)
-  r0.ChangeDutyCycle(0)
-  r1.ChangeDutyCycle(100)
+  L01.ChangeDutyCycle(0)
+  L10.ChangeDutyCycle(100)
+  R01.ChangeDutyCycle(0)
+  R10.ChangeDutyCycle(100)
 
 #makes the car spin and face left
 def spinleft():
-  l0.ChangeDutyCycle(0)
-  l1.ChangeDutyCycle(100)
-  r0.ChangeDutyCycle(100)
-  r1.ChageDutyCycle(0)
+  L01.ChangeDutyCycle(0)
+  L10.ChangeDutyCycle(100)
+  R01.ChangeDutyCycle(100)
+  R10.ChageDutyCycle(0)
 
 #makes car spin right
 def spinright():
-  l0.ChangeDutyCycle(100)
-  l1.ChangeDutyCycle(0)
-  r0.CHangeDutyCycle(0)
-  r1.ChangeDutyCycle(100)
+  L01.ChangeDutyCycle(100)
+  L10.ChangeDutyCycle(0)
+  R01.CHangeDutyCycle(0)
+  R10.ChangeDutyCycle(100)
 
 #makes car diagonally to the front (and left)
 def left():
-  l0.ChangeDutyCycle(60)
-  l1.ChangedutyCycle(0)
-  r0.ChangeDutyCycle(100)
-  r1.ChangeDutyCycle(0)
+  L01.ChangeDutyCycle(60)
+  L10.ChangedutyCycle(0)
+  R01.ChangeDutyCycle(100)
+  R10.ChangeDutyCycle(0)
 
 #makes car go diagonally to the front (and right)
 def right():
-  l0.ChangeDutyCycle(100)
-  l1.ChangeDutyCycle(0)
-  r0.ChageDutyCycle(60)
-  r1.ChangeDutyCycle(0)
+  L01.ChangeDutyCycle(100)
+  L10.ChangeDutyCycle(0)
+  R01.ChageDutyCycle(60)
+  R10.ChangeDutyCycle(0)
 
 #stops the car by not supplying a current to the motors
 def stop():
-  l0.stop()
-  l1.stop()
-  r0.stop()
-  r1.stop()
+  L01.stop()
+  L10.stop()
+  R01.stop()
+  R10.stop()
 
-# uninitializes all the gpio pins and returns all states to default
+#uninitializes all the gpio pins and returns all states to default
 def cleanup():
   stop()
   gpio.cleanup()
---
+++ RaspberryPi/RCcarFunctionsTEST.py
@@ -0,0 +1,19 @@
+import RCcarFunctions as RCcar, time
+while True:
+  RCcar.forward()
+  time.sleep(2)
+  
+  RCcar.reverse()
+  time.sleep(2)
+  
+  RCcar.left()
+  time.sleep(2)
+  
+  RCcar.right()
+  time.sleep(2)
+  
+  RCcar.stop()
+  time.sleep(2)
+
+
+
