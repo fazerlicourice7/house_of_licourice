01277feeaee308e439e2bdebfc486505d58901c0
Vamshi Balanaga <vamrocks602@gmail.com>
2015-08-12 15:15:37 -0400

    started gui interface

--- Anagram_Maker/src/anagramMaker/anagram_finder.java
+++ Anagram_Maker/src/anagramMaker/anagram_finder.java
@@ -11,26 +11,12 @@ public class anagram_finder {
     static BufferedReader READ = new BufferedReader(new InputStreamReader(System.in));
     static ArrayList permutations;
     static ArrayList WORDS;
-    
+
     public static void main(String args[]) throws IOException {
-        System.out.println("Enter a word");
-        String input = READ.readLine();
-        System.out.println();
+        String input;
+        input = Interface.getInput();
         permutations = getPermutations.permutations(input);
-        
         WORDS = checksDictionary.findWords(permutations);
-        if (WORDS.size() < 1) {
-            System.out.println("There are no anagrams of the string you entered, would you like all the permutations instead?" +"\n" +"'y' or 'n'");
-            String input2 = READ.readLine();
-            if(input2.equalsIgnoreCase("y")||input2.equalsIgnoreCase("yes")){
-                for(int loop = 0; loop <permutations.size(); loop++){
-                    System.out.println(permutations.get(loop));
-                }
-            }
-        } else {
-            for (int loop = 0; loop < WORDS.size(); loop++) {
-                System.out.println(WORDS.get(loop));
-            }
-        }
+        Interface.setOutput(WORDS, permutations);
     }
 }
