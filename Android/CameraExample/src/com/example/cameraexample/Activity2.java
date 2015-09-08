1abe1dd6f9bdba085b5ebc0b00717aabc1a21932
Vamshi Balanaga <vamrocks602@gmail.com>
2015-09-08 16:37:53 -0400

    I'm tired of git

---
+++ Android/CameraExample/src/com/example/cameraexample/Activity2.java
@@ -0,0 +1,61 @@
+<<<<<<< HEAD
+package com.example.cameraexample;
+
+import java.io.File;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+
+public class Activity2 extends Activity {
+	Intent media;
+    protected void onCreate(Bundle savedInstanceState){
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_end);
+	    media = getIntent();
+    }
+    
+    protected void save(){
+    	
+    }
+    
+    protected void delete(){
+    	Uri location = media.getData();
+    	File Location = new File(location.getPath());
+    	Location.delete();
+    }
+    
+   
+
+}
+=======
+package com.example.cameraexample;
+
+import java.io.File;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+
+public class Activity2 extends Activity {
+	Intent media;
+    protected void onCreate(Bundle savedInstanceState){
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_end);
+	    media = getIntent();
+    }
+    
+    protected void save(){
+    	
+    }
+    
+    protected void delete(){
+    	Uri location = media.getData();
+    	File Location = new File(location.getPath());
+    	Location.delete();
+    }
+    
+   
+
+}
+>>>>>>> 0d018ee7d24f8eba23d577420e595cccce1748ca
